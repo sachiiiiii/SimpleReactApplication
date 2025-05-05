900976c5bada585902295c9c298b52a5ec70494f
Osi <osachi752@gmail.com>
2025-05-05 10:05:43 -0400

    added another prop

--- src/components/Objectives.jsx
+++ src/components/Objectives.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Objectives(props) {
     return (
-        <ul style={{ fontSize: props.fontSize }}>
+        <ul style={{ fontSize: props.fontSize, listStyle: props.listStyle}}>
             <li>Use <span>npm create vite@latest</span> to make a pre-configured React application.</li>
             <li>Create React components.</li>
             <li>Render React components within an application.</li>
